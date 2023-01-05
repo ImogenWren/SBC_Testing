3915150ceda9fa01fef6139dcc1eba77d59624e1
ImogenWren <imogen.wren91@gmail.com>
2023-01-05 14:07:30 +0000

    dsfdg

---
+++ sendEmail.py
@@ -0,0 +1,26 @@
+# import packages
+# below packages are built-in - no need to install anything new!
+# yupi :)
+import smtplib
+from email.message import EmailMessage
+
+import os
+from dotenv import load_dotenv
+_ = load_dotenv()
+
+# set your email and password
+# please use App Password
+email_address = os.environ.get("EMAIL_ADDRESS")
+email_password = os.environ.get("EMAIL_PASSWORD")
+
+# create email
+msg = EmailMessage()
+msg['Subject'] = "Email subject"
+msg['From'] = email_address
+msg['To'] = "to-address@gmail.com"
+msg.set_content("This is eamil message")
+
+# send email
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(email_address, email_password)
+    smtp.send_message(msg)
