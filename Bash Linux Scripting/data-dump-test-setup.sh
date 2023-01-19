98eee755ccc8fb348bdf628930f546256e51f15a
Imogen Wren <imogen.wren91@gmail.com>
2023-01-19 15:17:49 +0000

    Update data-dump-test-setup.sh

--- Bash Linux Scripting/data-dump-test-setup.sh
+++ Bash Linux Scripting/data-dump-test-setup.sh
@@ -12,11 +12,12 @@
 
 cd /
 echo Cloning sbc-testing repo...
-git clone https://github.com/ImogenWren/SBC_Testing ./remotelabs/sbc-testing
+git clone https://github.com/ImogenWren/SBC_Testing /remotelabs/sbc-testing
 ls | grep sbc-testing
 echo copying python file
 cp ./remotelabs/sbc-testing/python-scripting/dataDump.py /home/imogen/dataDump.py
-chmod +x dataDump.py
+cd /
+sudo chmod +x dataDump.py
 echo Modifying Crontab
 echo "@reboot sudo python3 /home/imogen/dataDump.py" | sudo crontab
 sudo systemctl enable cron
