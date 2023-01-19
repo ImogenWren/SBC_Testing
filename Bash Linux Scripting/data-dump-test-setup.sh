98c5010065c2599fe27a3602e72a9f5a31781a5e
Imogen Wren <imogen.wren91@gmail.com>
2023-01-19 14:54:19 +0000

    Create data-dump-test-setup.sh

---
+++ Bash Linux Scripting/data-dump-test-setup.sh
@@ -0,0 +1,25 @@
+
+#!/bin/sh
+# data-dump-test-setup.sh
+# Navigate to home directory, then to this directory.
+# Then execute all commands nessissary to set up odroid for dataDump power cycle test
+# NOTE: Modify scriopts for rpi for testing
+# 1. add this file to home directory
+# 2. chmod +x data-dump-test-setup.sh
+# 3. sudo sh data-dump-test.sh
+# 
+
+
+cd /
+echo Cloning sbc-testing repo...
+git clone https://github.com/ImogenWren/SBC_Testing ./remotelabs/SBC_Testing
+echo copying python file
+cp ./remotelabs/sbc-testing/python-scripting/dataDump.py /home/odroid/dataDump.py
+chmod +x dataDump.py
+echo Modifying Crontab
+echo "@reboot sudo python3 /home/imogen/dataDump.py" | sudo crontab
+sudo systemctl enable cron
+nano genData.txt
+sudo reboot
+
+
