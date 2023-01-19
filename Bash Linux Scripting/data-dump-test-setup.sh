6770e99e88daaa03ec73edc1b8e182c6c8f9e850
Imogen Wren <imogen.wren91@gmail.com>
2023-01-19 15:19:11 +0000

    Update data-dump-test-setup.sh

--- Bash Linux Scripting/data-dump-test-setup.sh
+++ Bash Linux Scripting/data-dump-test-setup.sh
@@ -21,7 +21,7 @@ sudo chmod +x dataDump.py
 echo Modifying Crontab
 echo "@reboot sudo python3 /home/imogen/dataDump.py" | sudo crontab
 sudo systemctl enable cron
-echo date >> genData.txt
+date >> genData.txt
 cat genData.txt
 #sudo reboot
 
