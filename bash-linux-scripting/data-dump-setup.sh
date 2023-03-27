ae1ec94094a976340a86f5d6de0ae016c5eaa47b
ImogenWren <imogen.wren91@gmail.com>
2023-03-27 11:07:30 +0100

    added extra scripts

---
+++ bash-linux-scripting/data-dump-setup.sh
@@ -0,0 +1,31 @@
+
+#!/bin/sh
+# data-dump-test-od.sh
+# Navigate to home directory, then to this directory.
+# Then execute all commands nessissary to set up odroid for dataDump power cycle test
+# NOTE: Modiffied for Odroid & all files are copied offline
+# 1. add this file to home directory
+# 2. chmod +x data-dump-test-setup.sh
+# 3. sudo sh data-dump-test.sh
+# 
+
+
+cd /
+ls
+sleep 2
+echo Changing File Permissions
+sleep 2
+cd /
+sudo chmod +x /home/odroid/dataDump.py
+sleep 2
+echo Modifying Crontab
+sleep 1
+echo "@reboot sudo python3 /home/odroid/dataDump.py" | sudo crontab
+sudo systemctl enable cron
+sleep 2
+date >> /home/odroid/genData.txt
+cat genData.txt
+sleep 5
+echo Script Finished
+#sudo reboot
+
---
+++ bash-linux-scripting/send-setup-files.sh
@@ -0,0 +1,23 @@
+
+#!/bin/sh
+# send-setup-files.sh
+# send all files for data dump test - the python file and the bash install script
+# NOTE: Modiffied for Odroid
+# 1. Run this file from its folder on local machine, example command:
+# sh send-setup-files.sh user@host:destination/
+# 2. go to remote host and run data-dump-install.sh
+
+
+
+echo Copying Python File from Local To Host...
+scp /home/imogen/remotelabs/sbc-testing/python-scripting/dataDump.py $1
+sleep 2
+echo copying setup script file
+sleep 2
+scp /home/odroid/remotelabs/sbc-testing/bash-linux-scripting/data-dump-setup.sh $1
+sleep 2
+echo copying genData.txt file
+scp /home/odroid/remotelabs/sbc-testing/python-scripting/genData.txt $1
+echo Script Finished
+#sudo reboot
+
